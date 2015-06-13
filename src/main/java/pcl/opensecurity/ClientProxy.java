af9dba63f433019657a75cc299227060e9e1f148
Caitlyn <Michiyo.Ravencroft@gmail.com>
2015-06-12 20:37:29 -0500

    Removed rendering register stuff, doing solid blocks.

--- src/main/java/pcl/opensecurity/ClientProxy.java
+++ src/main/java/pcl/opensecurity/ClientProxy.java
@@ -1,32 +1,20 @@
 package pcl.opensecurity;
 
 
-import cpw.mods.fml.client.registry.ClientRegistry;
-import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
-import net.minecraftforge.client.MinecraftForgeClient;
 import pcl.opensecurity.CommonProxy;
 import pcl.opensecurity.containers.MagCardContainer;
-import pcl.opensecurity.renderers.BlockMagCardRenderer;
 import pcl.opensecurity.tileentity.TileEntityMagReader;
 import pcl.opensecurity.tileentity.TileEntityRFIDReader;
-import pcl.opensecurity.renderers.ItemMagComponentRenderer;
 
 public class ClientProxy extends CommonProxy {
 	
 	public void registerRenderers()
 	{
 		if (OpenSecurity.render3D) {
-			TileEntitySpecialRenderer MagCardRender = new BlockMagCardRenderer();
-			ClientRegistry.bindTileEntitySpecialRenderer(TileEntityMagReader.class, MagCardRender);
-			MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(OpenSecurity.magCardReader), new ItemMagComponentRenderer(MagCardRender, new TileEntityMagReader()));
 			
-			TileEntitySpecialRenderer RFIDCardRender = new BlockMagCardRenderer();
-			ClientRegistry.bindTileEntitySpecialRenderer(TileEntityRFIDReader.class, RFIDCardRender);
-			MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(OpenSecurity.rfidCardReader), new ItemMagComponentRenderer(RFIDCardRender, new TileEntityMagReader()));
 		}
 	}
 	
