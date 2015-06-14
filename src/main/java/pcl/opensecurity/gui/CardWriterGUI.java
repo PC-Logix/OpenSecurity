a6ecf40d98ca3a2b9f348a1ed5c688b7ccbf872d
Caitlyn <Michiyo.Ravencroft@gmail.com>
2015-06-14 02:54:50 -0500

    Unfux formatting

--- src/main/java/pcl/opensecurity/gui/CardWriterGUI.java
+++ src/main/java/pcl/opensecurity/gui/CardWriterGUI.java
@@ -12,27 +12,27 @@ import net.minecraft.util.StatCollector;
 
 public class CardWriterGUI extends GuiContainer {
 
-    public CardWriterGUI (InventoryPlayer inventoryPlayer, TileEntityCardWriter tileEntity) {
-        //the container is instanciated and passed to the superclass for handling
-        super(new RFIDWriterContainer(inventoryPlayer, tileEntity));
-}
+	public CardWriterGUI (InventoryPlayer inventoryPlayer, TileEntityCardWriter tileEntity) {
+		//the container is instanciated and passed to the superclass for handling
+		super(new RFIDWriterContainer(inventoryPlayer, tileEntity));
+	}
 
-@Override
-protected void drawGuiContainerForegroundLayer(int param1, int param2) {
-	//the parameters for drawString are: string, x, y, color
-	mc.fontRenderer.drawString(StatCollector.translateToLocal("gui.string.cardslot"), 64, ySize - 160, 4210752);
-	mc.fontRenderer.drawString(StatCollector.translateToLocal("gui.string.cardwriter"), 60, -12, 4210752);
-}
+	@Override
+	protected void drawGuiContainerForegroundLayer(int param1, int param2) {
+		//the parameters for drawString are: string, x, y, color
+		mc.fontRenderer.drawString(StatCollector.translateToLocal("gui.string.cardslot"), 64, ySize - 160, 4210752);
+		mc.fontRenderer.drawString(StatCollector.translateToLocal("gui.string.cardwriter"), 60, -12, 4210752);
+	}
 
-@Override
-protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3) {
-        //draw your Gui here, only thing you need to change is the path
-        ResourceLocation texture = new ResourceLocation(OpenSecurity.MODID, "textures/gui/rfidwriter.png");
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        this.mc.renderEngine.bindTexture(texture);
-        int x = (width - xSize) / 2;
-        int y = (height - 196) / 2;
-        this.drawTexturedModalRect(x, y, 0, 0, xSize, 196);
-}
-	
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3) {
+		//draw your Gui here, only thing you need to change is the path
+		ResourceLocation texture = new ResourceLocation(OpenSecurity.MODID, "textures/gui/rfidwriter.png");
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		this.mc.renderEngine.bindTexture(texture);
+		int x = (width - xSize) / 2;
+		int y = (height - 196) / 2;
+		this.drawTexturedModalRect(x, y, 0, 0, xSize, 196);
+	}
+
 }
