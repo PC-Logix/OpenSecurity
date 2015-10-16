95aa881ebd8cdc35b5695bb89ca2341498bab0e2
Caitlyn <Michiyo.Ravencroft@gmail.com>
2015-10-16 16:19:38 -0500

    Mag Reader can have it's event renamed (Default is still magData)

--- src/main/java/pcl/opensecurity/tileentity/TileEntityMagReader.java
+++ src/main/java/pcl/opensecurity/tileentity/TileEntityMagReader.java
@@ -1,6 +1,9 @@
 package pcl.opensecurity.tileentity;
 
 import li.cil.oc.api.Network;
+import li.cil.oc.api.machine.Arguments;
+import li.cil.oc.api.machine.Callback;
+import li.cil.oc.api.machine.Context;
 import li.cil.oc.api.network.ComponentConnector;
 import li.cil.oc.api.network.Environment;
 import li.cil.oc.api.network.Message;
@@ -18,6 +21,7 @@ import pcl.opensecurity.items.ItemMagCard;
 public class TileEntityMagReader extends TileEntityMachineBase implements Environment {
 
 	public String data;
+	public String eventName = "magData";
 
 	protected ComponentConnector node = Network.newNode(this, Visibility.Network).withComponent(getComponentName()).withConnector(32).create();
 
@@ -67,16 +71,23 @@ public class TileEntityMagReader extends TileEntityMachineBase implements Enviro
 		if (node != null && node.host() == this) {
 			node.load(nbt.getCompoundTag("oc:node"));
 		}
+		if (!(nbt.getString("eventName") == null)) {
+			eventName = nbt.getString("eventName");
+		} else {
+			eventName = "magData";
+		}
 	}
 
 	@Override
 	public void writeToNBT(NBTTagCompound nbt) {
 		super.writeToNBT(nbt);
+		
 		if (node != null && node.host() == this) {
 			final NBTTagCompound nodeNbt = new NBTTagCompound();
 			node.save(nodeNbt);
 			nbt.setTag("oc:node", nodeNbt);
 		}
+		nbt.setString("eventName", eventName);
 	}
 
 	public boolean doRead(ItemStack itemStack, EntityPlayer em) {
@@ -85,7 +96,7 @@ public class TileEntityMagReader extends TileEntityMachineBase implements Enviro
 			String uuid = itemStack.stackTagCompound.getString("uuid");
 			boolean locked = itemStack.stackTagCompound.getBoolean("locked");
 			if (node.changeBuffer(-5) == 0) {
-				node.sendToReachable("computer.signal", "magData", em.getDisplayName(), data, uuid, locked);
+				node.sendToReachable("computer.signal", eventName, em.getDisplayName(), data, uuid, locked);
 			}
 			return true;
 		} else {
@@ -93,6 +104,17 @@ public class TileEntityMagReader extends TileEntityMachineBase implements Enviro
 		}
 	}
 
+	@Callback
+	public Object[] greet(Context context, Arguments args) {
+		return new Object[] { "Lasciate ogne speranza, voi ch'intrate" };
+	}
+
+	@Callback(doc = "function(String:name):boolean; Sets the name of the event that gets sent when a card is swipped", direct = true)
+	public Object[] setEventName(Context context, Arguments args) throws Exception {
+		eventName = args.checkString(0);
+		return new Object[]{ true };
+	}
+	
 	@Override
 	public void updateEntity() {
 		super.updateEntity();
