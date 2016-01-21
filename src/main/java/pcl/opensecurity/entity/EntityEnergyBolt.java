17ebcfd1823aacae6c7b870e0fb620973aeae7c4
Caitlyn <Michiyo.Ravencroft@gmail.com>
2016-01-21 12:26:35 -0600

    Closes #33 setVelocity is marked Side.CLIENT and stripped from dedicated servers

--- src/main/java/pcl/opensecurity/entity/EntityEnergyBolt.java
+++ src/main/java/pcl/opensecurity/entity/EntityEnergyBolt.java
@@ -33,7 +33,11 @@ public class EntityEnergyBolt
   public void setHeading(float yaw, float pitch)
   {
     DataWatcher d = getDataWatcher();
-    setVelocity((float)(-Math.sin(yaw) * Math.cos(pitch)), (float)-Math.sin(pitch), (float)(Math.cos(yaw) * Math.cos(pitch)));
+  //This will crash when run on a dedicated server, because Entity#setVelocity is marked with @SideOnly(Side.CLIENT), and therefore stripped from a dedicated server instance.
+    //setVelocity((float)(-Math.sin(yaw) * Math.cos(pitch)), (float)-Math.sin(pitch), (float)(Math.cos(yaw) * Math.cos(pitch)));
+    this.motionX = -Math.sin(yaw) * Math.cos(pitch);
+    this.motionY = -Math.sin(pitch);
+    this.motionZ = Math.cos(yaw) * Math.cos(pitch);
     d.updateObject(19, Float.valueOf(this.yaw = yaw));
     d.updateObject(20, Float.valueOf(this.pitch = pitch));
   }
