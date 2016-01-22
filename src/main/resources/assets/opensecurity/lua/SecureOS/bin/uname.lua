8f87bfafbe3dcd0d86090899f27225fe0ee37f8e
Caitlyn <Michiyo.Ravencroft@gmail.com>
2016-01-21 19:54:11 -0600

    Remove SecureOS

--- src/main/resources/assets/opensecurity/lua/SecureOS/bin/uname.lua
+++
@@ -1,7 +0,0 @@
-local args, options = require("shell").parse(...)
-
-if options.a then
-  io.write(_OSVERSION .. " " .. _VERSION)
-else
-  io.write(_OSVERSION)
-end
--- src/main/resources/assets/opensecurity/lua/SecureOS/bin/whoami.lua
+++
@@ -1,1 +0,0 @@
-print(os.getenv('USER'))
