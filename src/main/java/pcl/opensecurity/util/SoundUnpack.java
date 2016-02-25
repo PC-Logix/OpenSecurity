a61778cbcbefc2b169c6adee89cab4a69d65e19f
Caitlyn Mainer <michiyo.ravencroft@gmail.com>
2016-02-24 22:37:15 -0600

    Pack alarms in jar and extract on run

--- src/main/java/pcl/opensecurity/util/SoundUnpack.java
+++ src/main/java/pcl/opensecurity/util/SoundUnpack.java
@@ -1,18 +1,34 @@
 package pcl.opensecurity.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Map;
 
+import pcl.opensecurity.BuildInfo;
 import cpw.mods.fml.relauncher.IFMLCallHook;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 
 public class SoundUnpack implements IFMLLoadingPlugin, IFMLCallHook {
-	
+
 	@SuppressWarnings("static-access")
 	public void load() {
-		new SoundUnpack().getClass().getClassLoader().getSystemResource(this.getClass().getName() + ".class");
-		
-		new ExtractDirectory("jar:/assets/opensecurity/sounds/alarms/", "mods/OpenSecurity/sounds/alarms");
+		File f = new File("\\mods\\");
+		File jar = null;
+		try {
+			jar = new File(SoundUnpack.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
+		} catch (URISyntaxException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			new ExtractDirectory(new URL("file:/" + jar  + "!/assets/opensecurity/sounds/alarms/"), "mods/OpenSecurity/sounds/alarms");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
