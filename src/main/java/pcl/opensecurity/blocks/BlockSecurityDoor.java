4cbd3d83608f4a07fb43a8e3154757e108c16a2c
Caitlyn Mainer <michiyo.ravencroft@gmail.com>
2016-05-01 21:25:29 -0500

    Switch door block material

--- src/main/java/pcl/opensecurity/blocks/BlockSecurityDoor.java
+++ src/main/java/pcl/opensecurity/blocks/BlockSecurityDoor.java
@@ -33,7 +33,7 @@ public class BlockSecurityDoor extends BlockDoor {
 
 	public BlockSecurityDoor()
 	{
-		super(Material.anvil);
+		super(Material.iron);
 		float f = 0.5F;
 		float f1 = 1.0F;
 		this.setBlockBounds(0.5F - f, 0.0F, 0.5F - f, 0.5F + f, f1, 0.5F + f);
