5f8d86ffec376de28ba4f2d55f8193ef869726e4
Caitlyn Mainer <michiyo.ravencroft@gmail.com>
2016-08-18 23:00:48 -0500

    This is a kludge, but it works.

--- src/main/java/pcl/opensecurity/util/SoundUnpack.java
+++ src/main/java/pcl/opensecurity/util/SoundUnpack.java
@@ -1,28 +1,53 @@
 package pcl.opensecurity.util;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.InputStream;
+import java.util.Enumeration;
 import java.util.Map;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import pcl.opensecurity.BuildInfo;
 import cpw.mods.fml.relauncher.IFMLCallHook;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 
 public class SoundUnpack implements IFMLLoadingPlugin, IFMLCallHook {
-	public void load() {
-		File jar = null;
+	public void load() throws IOException {
+		File f = new File("mods/OpenSecurity/sounds/alarms/");
+		f.mkdirs();
+		final String path = "assets/opensecurity/sounds/alarms";
+		final File jarFile = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+		if(jarFile.isFile()) {  // Run with JAR file
+		    JarFile jar = null;
+			jar = new JarFile(jarFile);
+		    final Enumeration<JarEntry> entries = jar.entries(); //gives ALL entries in jar
+		    while(entries.hasMoreElements()) {
+		        final String name = entries.nextElement().getName();
+		        if (name.startsWith(path + "/") && name.endsWith(".ogg")) { //filter according to the path
+		        	InputStream ddlStream = SoundUnpack.class.getClassLoader().getResourceAsStream(name);
+		        	Path p = Paths.get(name);
+		        	String file = p.getFileName().toString();
+		        		try (FileOutputStream fos = new FileOutputStream(f + "/" + file);){
+		        		    byte[] buf = new byte[2048];
+		        		    int r;
+		        		    while(-1 != (r = ddlStream.read(buf))) {
+		        		        fos.write(buf, 0, r);
+		        		    }
+		        		}
+		        	
+		        }
+		    }
+			jar.close();
+		}
+/*		File jar = null;
 		File f = new File("mods/OpenSecurity/sounds/");
-		//if (!f.exists()) {
+		if (!f.exists()) {
 			f.mkdirs();
 			try {
-				Path path = Paths.get(SoundUnpack.class.getProtectionDomain().getCodeSource().getLocation().toURI());
-				jar = new File(path.toString());
-				//jar = new File(SoundUnpack.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
+				jar = new File(SoundUnpack.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
 				System.out.println("Selected Jar");
 			} catch (URISyntaxException e) {
 				// TODO Auto-generated catch block
@@ -36,7 +61,7 @@ public class SoundUnpack implements IFMLLoadingPlugin, IFMLCallHook {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		//}
+		}*/
 	}
 
 	@Override
@@ -60,7 +85,12 @@ public class SoundUnpack implements IFMLLoadingPlugin, IFMLCallHook {
 
 	@Override
 	public Void call() {
-		load();
+		try {
+			load();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		return null;
 	}
