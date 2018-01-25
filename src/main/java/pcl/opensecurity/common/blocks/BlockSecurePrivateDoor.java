b38f4cb10cc5f9c8f4245d6f40e40b75b71fbf88
Caitlyn <Michiyo.Ravencroft@gmail.com>
2018-01-25 13:13:00 -0600

    Update BlockSecurePrivateDoor.java

--- src/main/java/pcl/opensecurity/common/blocks/BlockSecurePrivateDoor.java
+++ src/main/java/pcl/opensecurity/common/blocks/BlockSecurePrivateDoor.java
@@ -1,7 +1,15 @@
 package pcl.opensecurity.common.blocks;
 
-import net.minecraft.block.material.Material;
+import java.util.Random;
 
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import pcl.opensecurity.common.ContentRegistry;
 public class BlockSecurePrivateDoor extends BlockSecureDoor {
 
 	public BlockSecurePrivateDoor(Material materialIn) {
@@ -9,5 +17,19 @@ public class BlockSecurePrivateDoor extends BlockSecureDoor {
 		setUnlocalizedName("private_secure_door");
 		setHardness(.5f);
 	}
+	/**
+	* Get the Item that this Block should drop when harvested.
+	*/
+	@Nullable
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        	return state.getValue(HALF) == BlockSecureDoor.EnumDoorHalf.UPPER ? null : this.getItem();
+	}
+	
+	public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state) {
+        	return new ItemStack(this.getItem());
+	}
 
+	private Item getItem() {
+		return new ItemStack(ContentRegistry.privateSecureDoor).getItem();
+	}
 }
