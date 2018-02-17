db42998dbbc70739d6fde38c44b585a638c97c1e
Caitlyn Mainer <michiyo.ravencroft@gmail.com>
2018-02-17 01:50:04 -0600

    Implement allowed users list.

--- src/main/java/pcl/opensecurity/common/OSBreakEvent.java
+++ src/main/java/pcl/opensecurity/common/OSBreakEvent.java
@@ -1,16 +1,14 @@
 package pcl.opensecurity.common;
 
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
-import pcl.opensecurity.OpenSecurity;
-import pcl.opensecurity.common.tileentity.TileEntityAlarm;
-import pcl.opensecurity.common.tileentity.TileEntityDoorController;
-import pcl.opensecurity.common.tileentity.TileEntitySecureDoor;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import pcl.opensecurity.OpenSecurity;
+import pcl.opensecurity.common.tileentity.TileEntityDoorController;
+import pcl.opensecurity.common.tileentity.TileEntitySecureDoor;
 import pcl.opensecurity.common.tileentity.TileEntitySecurityTerminal;
 
 public class OSBreakEvent {
@@ -25,14 +23,14 @@ public class OSBreakEvent {
 			TileEntity TE = event.getWorld().getTileEntity(event.getPos());
 			if(TE instanceof TileEntitySecureDoor){
 				TileEntitySecureDoor xEntity = (TileEntitySecureDoor) TE;
-				if(xEntity.getOwner()!=null && !xEntity.getOwner().equals(event.getPlayer().getUniqueID().toString()) && !event.getPlayer().capabilities.isCreativeMode && !xEntity.getOwner().isEmpty()){
+				if(xEntity.getOwner()!=null && !xEntity.getOwner().equals(event.getPlayer().getUniqueID().toString()) && !xEntity.getOwner().isEmpty()){
 					if (!event.getPlayer().canUseCommand(2,"")) {
 						event.setCanceled(true);
 					}
 				}
 			} else if(TE instanceof TileEntityDoorController){
 				TileEntityDoorController xEntity = (TileEntityDoorController) TE;
-				if(xEntity.getOwner()!=null && !xEntity.getOwner().equals(event.getPlayer().getUniqueID().toString()) && !event.getPlayer().capabilities.isCreativeMode && !xEntity.getOwner().isEmpty()){
+				if(xEntity.getOwner()!=null && !xEntity.getOwner().equals(event.getPlayer().getUniqueID().toString()) && !xEntity.getOwner().isEmpty()){
 					if (!event.getPlayer().canUseCommand(2,"")) {
 						event.setCanceled(true);
 					}
@@ -41,12 +39,11 @@ public class OSBreakEvent {
 
             BlockPos eventPos = event.getPos();
             Iterable<BlockPos> blocks = BlockPos.getAllInBox(eventPos.add(-8.0f, -8.0f, -8.0f), eventPos.add(8.0f, 8.0f, 8.0f));
-
+			World world = event.getWorld();
             for (BlockPos pos : blocks) {
-                World world = event.getWorld();
                 if(world.getTileEntity(pos) instanceof TileEntitySecurityTerminal){ // Not sure what to put here <<
                     TileEntitySecurityTerminal xEntity = (TileEntitySecurityTerminal) world.getTileEntity(pos);
-                    if(xEntity.getOwner()!=null && !xEntity.getOwner().equals(event.getPlayer().getUniqueID().toString()) && !event.getPlayer().capabilities.isCreativeMode && !xEntity.getOwner().isEmpty()){
+                    if(xEntity.getOwner()!=null && !xEntity.isUserAllowedToBypass(event.getPlayer().getUniqueID().toString()) && !xEntity.getOwner().isEmpty()){
                         if (!event.getPlayer().canUseCommand(2,"") && xEntity.isEnabled()) {
                             xEntity.usePower();
                             event.setCanceled(true);
--- src/main/java/pcl/opensecurity/common/tileentity/TileEntitySecurityTerminal.java
+++ src/main/java/pcl/opensecurity/common/tileentity/TileEntitySecurityTerminal.java
@@ -1,5 +1,6 @@
 package pcl.opensecurity.common.tileentity;
 
+import com.mojang.authlib.GameProfile;
 import li.cil.oc.api.Network;
 import li.cil.oc.api.machine.Arguments;
 import li.cil.oc.api.machine.Callback;
@@ -10,6 +11,9 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.world.WorldServer;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class TileEntitySecurityTerminal extends TileEntityOSBase {
     public void setOwner(String UUID) {
         this.ownerUUID = UUID;
@@ -19,13 +23,18 @@ public class TileEntitySecurityTerminal extends TileEntityOSBase {
         return this.ownerUUID;
     }
     String ownerUUID = "";
+    ArrayList<String> allowedUsers;
     private String password = "";
     public Block block;
     private Boolean enabled = false;
     boolean enableParticles = false;
 
     public TileEntitySecurityTerminal(){
-        node = Network.newNode(this, Visibility.Network).withComponent(getComponentName()).withConnector(32).create();
+        node = Network.newNode(this, Visibility.Network).withComponent(getComponentName()).withConnector(128).create();
+    }
+
+    public boolean isUserAllowedToBypass(String uuid) {
+        return uuid.equals(ownerUUID) || allowedUsers.contains(uuid);
     }
 
     private static String getComponentName() {
@@ -35,6 +44,48 @@ public class TileEntitySecurityTerminal extends TileEntityOSBase {
     @Callback
     public Object[] isEnabled(Context context, Arguments args) {
         return new Object[] { isEnabled() };
+    }
+
+    @Callback
+    public Object[] addUser(Context context, Arguments args) {
+        if (args.checkString(0).equals(getPass())) {
+            if (args.checkString(1).matches("[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}")) {
+                allowedUsers.add(args.checkString(1));
+            } else {
+                GameProfile gameprofile = world.getMinecraftServer().getPlayerProfileCache().getGameProfileForUsername(args.checkString(1));
+
+                if (gameprofile == null)
+                {
+                    return new Object[] { true, "Failed to get UUID from username" };
+                } else {
+                    allowedUsers.add(gameprofile.getId().toString());
+                }
+            }
+            return new Object[] { true, "User added" };
+        } else {
+            return new Object[] { false, "Password was incorrect" };
+        }
+    }
+
+    @Callback
+    public Object[] delUser(Context context, Arguments args) {
+        if (args.checkString(0).equals(getPass())) {
+            if (args.checkString(1).matches("[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}")) {
+                allowedUsers.remove(args.checkString(1));
+            } else {
+                GameProfile gameprofile = world.getMinecraftServer().getPlayerProfileCache().getGameProfileForUsername(args.checkString(1));
+
+                if (gameprofile == null)
+                {
+                    return new Object[] { true, "Failed to get UUID from username" };
+                } else {
+                    allowedUsers.remove(gameprofile.getId().toString());
+                }
+            }
+            return new Object[] { true, "User removed" };
+        } else {
+            return new Object[] { false, "Password was incorrect" };
+        }
     }
 
     @Callback
@@ -207,6 +258,7 @@ public class TileEntitySecurityTerminal extends TileEntityOSBase {
         this.ownerUUID = nbt.getString("owner");
         this.password= nbt.getString("password");
         this.enabled=nbt.getBoolean("enabled");
+        this.allowedUsers=new ArrayList<String>(Arrays.asList(nbt.getString("allowedUsers").replaceAll(", $", "").split(", ")));
     }
 
     @Override
@@ -220,6 +272,8 @@ public class TileEntitySecurityTerminal extends TileEntityOSBase {
         nbt.setString("owner", this.ownerUUID);
         nbt.setString("password", this.password);
         nbt.setBoolean("enabled", this.isEnabled());
+        if (this.allowedUsers != null && this.allowedUsers.size() > 0)
+            nbt.setString("allowedUsers", String.join(", ", this.allowedUsers).replaceAll(", $", ""));
         return nbt;
     }
 
