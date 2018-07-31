41ffd32979ead5f259c9627c1271b91103d2220c
Caitlyn <Michiyo.Ravencroft@gmail.com>
2018-07-31 15:44:37 -0700

    Close #105 for 1.7

--- src/main/java/pcl/opensecurity/tileentity/TileEntityKeypadLock.java
+++ src/main/java/pcl/opensecurity/tileentity/TileEntityKeypadLock.java
@@ -181,7 +181,7 @@ public class TileEntityKeypadLock extends TileEntityMachineBase implements Envir
 				}
 				if (colors!=null)
 				{
-					val = colors.get(id);
+					val = Integer.parseInt((String) colors.get(id));
 					if(val!=null && val instanceof Number)
 					{
 						Number color = (Number)val;
