e1b22fa0a87980f404334632fbb88547c8283175
ben-mkiv <advoztyerusdowranhivcin2@gmail.com>
2018-12-31 15:27:24 +0100

    more abstraction -.-

--- src/main/java/pcl/opensecurity/networking/PacketProtectionSync.java
+++ src/main/java/pcl/opensecurity/networking/PacketProtectionSync.java
@@ -8,10 +8,8 @@ import net.minecraftforge.fml.common.network.ByteBufUtils;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
-import pcl.opensecurity.OpenSecurity;
 import pcl.opensecurity.common.protection.Protection;
 
-import java.util.logging.Logger;
 
 public class PacketProtectionSync implements IMessage {
     NBTTagCompound protection;
