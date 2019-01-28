1f39cd8fc5599d77f1c6cbea8a017954ad21d257
ben-mkiv <advoztyerusdowranhivcin2@gmail.com>
2019-01-28 02:42:32 +0100

    unnecessary import removed -.-

--- src/main/java/pcl/opensecurity/client/renderer/RenderKeypad.java
+++ src/main/java/pcl/opensecurity/client/renderer/RenderKeypad.java
@@ -1,7 +1,6 @@
 package pcl.opensecurity.client.renderer;
 
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
