44ee136da5b5abe1dacc27d09bb4ac3f8bf61f7c
ben-mkiv <advoztyerusdowranhivcin2@gmail.com>
2019-06-26 13:29:14 +0200

    dont register manual content on dedicated server startup

--- src/main/java/pcl/opensecurity/manual/ManualPathProviderOC.java
+++ src/main/java/pcl/opensecurity/manual/ManualPathProviderOC.java
@@ -4,11 +4,15 @@ import li.cil.oc.api.Manual;
 import li.cil.oc.api.manual.PathProvider;
 import li.cil.oc.api.prefab.TextureTabIconRenderer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.relauncher.Side;
 
-public class ManualPathProviderOC extends ManualPathProvider implements PathProvider {
-    public void initialize(ResourceLocation iconResourceLocation, String tooltip, String path) {
-        Manual.addProvider(new ManualPathProviderOC());
-        Manual.addProvider(new ManualContentProviderOC());
-        Manual.addTab(new TextureTabIconRenderer(iconResourceLocation), tooltip, path);
+class ManualPathProviderOC extends ManualPathProvider implements PathProvider {
+    void initialize(ResourceLocation iconResourceLocation, String tooltip, String path) {
+        if(FMLCommonHandler.instance().getEffectiveSide().equals(Side.CLIENT)) {
+            Manual.addProvider(new ManualPathProviderOC());
+            Manual.addProvider(new ManualContentProviderOC());
+            Manual.addTab(new TextureTabIconRenderer(iconResourceLocation), tooltip, path);
+        }
     }
 }
--- src/main/java/pcl/opensecurity/manual/ManualPathProviderRTFM.java
+++ src/main/java/pcl/opensecurity/manual/ManualPathProviderRTFM.java
@@ -6,24 +6,28 @@ import li.cil.manual.api.manual.PathProvider;
 import li.cil.manual.api.prefab.manual.TextureTabIconRenderer;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.relauncher.Side;
 import pcl.opensecurity.OpenSecurity;
 
-public class ManualPathProviderRTFM extends ManualPathProvider implements PathProvider {
-    static ManualDefinition manual;
+class ManualPathProviderRTFM extends ManualPathProvider implements PathProvider {
+    private static ManualDefinition manual;
 
-    public void initialize(ResourceLocation iconResourceLocation, String tooltip, String path) {
+    void initialize(ResourceLocation iconResourceLocation, String tooltip, String path) {
         manual = ManualAPI.createManual(false);
 
-        manual.addProvider(new ManualPathProviderRTFM());
-        manual.addProvider(new ManualContentProviderRTFM());
-        manual.setDefaultPage(path);
+        if(FMLCommonHandler.instance().getEffectiveSide().equals(Side.CLIENT)) {
+            manual.addProvider(new ManualPathProviderRTFM());
+            manual.addProvider(new ManualContentProviderRTFM());
+            manual.setDefaultPage(path);
 
-        manual.addTab(new TextureTabIconRenderer(iconResourceLocation), tooltip, path);
-        manual.addTab(new TextureTabIconRenderer(new ResourceLocation(OpenSecurity.MODID, "textures/blocks/door_controller.png")), "Blocks", "assets/" + OpenSecurity.MODID + "/doc/Blocks");
-        manual.addTab(new TextureTabIconRenderer(new ResourceLocation(OpenSecurity.MODID, "textures/items/mag_card.png")), "Items", "assets/" + OpenSecurity.MODID + "/doc/Items");
+            manual.addTab(new TextureTabIconRenderer(iconResourceLocation), tooltip, path);
+            manual.addTab(new TextureTabIconRenderer(new ResourceLocation(OpenSecurity.MODID, "textures/blocks/door_controller.png")), "Blocks", "assets/" + OpenSecurity.MODID + "/doc/Blocks");
+            manual.addTab(new TextureTabIconRenderer(new ResourceLocation(OpenSecurity.MODID, "textures/items/mag_card.png")), "Items", "assets/" + OpenSecurity.MODID + "/doc/Items");
+        }
     }
 
-    public static Item getManualItem(){
+    static Item getManualItem(){
         return ManualAPI.createItemForManual(manual);
     }
 
